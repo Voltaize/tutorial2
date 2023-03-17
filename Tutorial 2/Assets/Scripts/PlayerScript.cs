0f41b9fc903c1716d567933d0c6fcc6d0f8bb5a4
Voltaize <nflodquist@knights.ucf.edu>
2023-03-17 18:07:12 -0400

    part 3

--- Tutorial 2/Assets/Scripts/PlayerScript.cs
+++ Tutorial 2/Assets/Scripts/PlayerScript.cs
@@ -11,8 +11,11 @@ public class PlayerScript : MonoBehaviour
 
     public Text score;
     public Text Win;
+    public Text Lives;
 
     private int scoreValue = 0;
+    private int livesValue = 3;
+
     private int move = 0;
 
     public AudioClip musicClipOne;
@@ -20,13 +23,16 @@ public class PlayerScript : MonoBehaviour
     public AudioSource musicSource;
 
     private bool facingRight = true;
+     private int gameover = 0;
     Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         rd2d = GetComponent<Rigidbody2D>();
         score.text = scoreValue.ToString();
+        Lives.text = livesValue.ToString();
         musicSource.clip = musicClipOne;
         musicSource.Play();
     }
@@ -49,6 +55,17 @@ public class PlayerScript : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+         if (collision.collider.tag == "Enemy")
+        {
+            livesValue -= 1;
+            Lives.text = livesValue.ToString();
+            if(livesValue == 0)
+            {
+                 Win.text = "You lose! Game by Nicole F ";
+
+            }
+        }
+
        if (collision.collider.tag == "Coin")
         {
             scoreValue += 1;
@@ -62,15 +79,22 @@ public class PlayerScript : MonoBehaviour
         {
         transform.position = new Vector3(-17.0f, 0.5f, 3.0f);
            move = 1;
+           anim.SetInteger("State", 1);
         }
           
        }
         if (scoreValue == 8)
         {
+       
         musicSource.clip = musicClipTwo;
+        if (gameover == 0)
+        {
+        gameover = 1;
         musicSource.Play();
         musicSource.loop = false;
-        //win.text = "You win! Game by Nicole F ";
+        Win.text = "You win! Game by Nicole F ";
+        }
+        
 
         }
 
@@ -78,13 +102,17 @@ public class PlayerScript : MonoBehaviour
 
     private void OnCollisionStay2D(Collision2D collision)
     {
+        
         if (collision.collider.tag == "Ground")
         {
+            anim.SetInteger("State", 0);
             if (Input.GetKey(KeyCode.W))
             {
+                anim.SetInteger("State", 3);
                 rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse); //the 3 in this line of code is the player's "jumpforce," and you change that number to get different jump behaviors.  You can also create a public variable for it and then edit it in the inspector.
-                 anim.SetInteger("State", 3);
+                 
             }
+           
         }
     }
     void Flip()
